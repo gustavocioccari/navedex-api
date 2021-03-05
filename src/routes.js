c3305b4903166239f678792481ac0e15e74ddd75
gustavocioccari <gustavocioccari@gmail.com>
2021-03-05 10:25:54 -0300

    Add auth middleware to routes

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,15 @@ const routes = express.Router()
 const userController = require('./controllers/userController')
 const authController = require('./controllers/authController')
 
+const authMiddleware = require('./middlewares/authenticator')
+
 routes.get('/',(req, res) => {
   res.send('Hello World!')
 })
 
 routes.post('/user', userController.create)
 routes.post('/auth', authController.auth)
+
+routes.use(authMiddleware)
 
 module.exports = routes
