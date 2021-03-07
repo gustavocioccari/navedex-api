0a6799e17f81e41e996219ccc318231f499aebc0
gustavocioccari <gustavocioccari@gmail.com>
2021-03-06 21:43:55 -0300

    Update create and update in project service to associate navers to a project

--- src/services/Project.js
+++ src/services/Project.js
@@ -16,7 +16,27 @@ const Project = {
   },
 
   create: async function(projectData){
-    const project = await connection('projects').insert(projectData)
+    const { user_id, name, navers } = projectData
+    
+    const projectinsert = await connection('projects').insert({name:name,user_id:user_id})
+
+    const projectId = projectinsert[0]
+
+    if (!navers){
+      const project = await Project.findById(projectId)
+      return project
+    }
+
+    const naverProject = navers.map((naverId) => {
+      return {
+        naver_id: naverId,
+        project_id: projectId,
+      }
+    })
+
+    await connection('navers_projects').insert(naverProject)
+    
+    const project = await Project.findById(projectId)
 
     return project
   },
@@ -28,10 +48,26 @@ const Project = {
   },
 
   updateById: async function(id,updateData){
-    await connection('projects').update(updateData).where({ id })
-
-    const project = await Project.findById(id)
+    const { name, navers } = updateData
     
+    if (name)
+      await connection('projects').update({name}).where({ id })
+
+    if (!navers){      
+      const project = await Project.findById(id)
+      return project
+    }
+
+    const naverProject = navers.map((naverId) => {
+      return {
+        naver_id: naverId,
+        project_id: id,
+      }
+    })
+
+    await connection('navers_projects').insert(naverProject)
+    const project = await Project.findById(id)
+      
     return project
   }
 }
